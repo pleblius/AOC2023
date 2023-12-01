11c9c5e30ffe83f87e888e05f3a2bf5a73910ea0
pleblius <pleblius@gmail.com>
2023-12-01 01:25:35 -0700

    Day 1 Part 2 Finished

--- day1.py
+++ day1.py
@@ -1,12 +1,39 @@
-import re
+import regex as re
 
 
 def parse_string(string):
-    digits = re.findall("[0-9]", string)
+    digits = parse_advanced(string)
     digit1 = digits[0]
     digit2 = digits[-1]
 
-    return 10 * int(digit1) + int(digit2)
+    int1 = get_int(digit1)
+    int2 = get_int(digit2)
+
+    return 10 * int1 + int2
+
+
+def get_int(digit):
+    num_dict = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    }
+
+    if digit in num_dict.keys():
+        return num_dict[digit]
+    else:
+        return int(digit)
+
+
+def parse_advanced(string):
+    pattern = "one|two|three|four|five|six|seven|eight|nine|[0-9]"
+    return re.findall(pattern, string, overlapped=True)
 
 
 f = open("files/day1input.txt")
